d7c20cc2e3f9bc5dc814d1f5be9b3b8fa68bb62b
noamm19-meet <noamm19@meet.mit.edu>
2018-02-19 19:49:35 +0200

    yes

--- gr_proj_shelly.py
+++ gr_proj_shelly.py
@@ -1,7 +1,8 @@
 import turtle
 import time
 import random
-from block import Block
+from block_shelly import Block
+from ball_shelly import Ball
 import math
 
 turtle.speed(0)
@@ -33,11 +34,39 @@ for i in range(NUMBER_OF_BLOCKS_COLUMN):
 	t=t+25
 	l=-290
 
+red=random.randint(0,255)
+green=random.randint(0,255)
+blue=random.randint(0,255)
+
+
+MY_BALL=Ball(0,-SCREEN_HEIGHT+45,3,3,10, (red,green,blue))
+def move_all_balls():
+	MY_BALL.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+def collide(a,b,c,d):
+	y=a.ycor()
+	x=a.xcor()
+	y2=b.ycor()
+	x1=b.xcor()
+	sqrt=(y2-y)*(y2-y)+(x1-x)*(x1-x)
+	des=math.sqrt(sqrt)
+	if(des+10<=c+d):
+		return True
+	else:
+		return False
+if(collide(MY_BALL,platform,MY_BALL.r,-SCREEN_HEIGHT+40)):
+	MY_BALL.dx=-1*MY_BALL.dx
+	MY_BALL.dy=-1*MY_BALL.dy
+
 def movearound(event):
 	platform.goto(event.x-SCREEN_WIDTH,-SCREEN_HEIGHT+20)
 
 turtle.getcanvas().bind("<Motion>",movearound)
 turtle.getscreen().listen()
+
+while True:
+	move_all_balls()
+
 
 	
 turtle.mainloop()
