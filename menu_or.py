0ab67e2c972604f4997661ea05db13f7fb85a935
Malak18-Meet <malak.jabarin@gmail.com>
2018-02-25 17:20:09 +0200

    hoed

--- menu_or.py
+++ menu_or.py
@@ -1,21 +1,18 @@
 import turtle 
 ans = True
+turtle.ht()
+turtle.tracer(0)
+turtle.penup()
+turtle.goto (-100,-100)
 turtle.write("""
 		1. INSTRUCTIONS
 		2. PLAY!!
 
 
-		PRESS ENTER TO QUIT
-		""",True, align="center", ("Times", 24, "bold italic"))
-while ans:
-	print("""
-		1. INSTRUCTIONS
-		2. PLAY!!
-
 
 		PRESS ENTER TO QUIT
-		""")
+		""",True, align="center", font=("Helvetica", 24, "bold italic"))
+
 
 	
-	turtle.mainloop()
-
+turtle.mainloop()
