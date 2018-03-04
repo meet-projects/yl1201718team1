c792813748a117af6422193a60f8e6fbfe1eb53b
Malak18-Meet <malak.jabarin@gmail.com>
2018-03-04 19:21:31 +0200

    add menu

--- breakout.py
+++ breakout.py
@@ -11,20 +11,33 @@ tracer(0)
 
 IS_GAME_OVER = False
 
-i=0
-def inst(event):
 
-	inn=turtle.clone()
-	inn.home()
+def inst(event):
 	turtle.clear()
-	turtle.addshape("in.gif")
-	inn.shape("in.gif")
-	inn.stamp()
+	turtle.goto(300,-250)
+	turtle.write("""
+		A BRICK WALL APPEARS AT THE SCREEN 
+		AND OUR MISSION IS TO SMASH IT
+		ONE BREAK AT A TIME!
+
+		*use the mouse to move the platform
+		across the bottom screen
+
+		*across the bottom screen
+		each time the ball hits a break, 
+		the brick disappears 
+		and you score points
+
+		2. PLAY!!
+
+		PRESS ENTER TO QUIT
+		""",True, align="right", font=("Helvetica", 15,"bold italic"))
 	turtle.update()
 def play(event):
 	turtle.clear()
 	turtle.update()
-	i=1
+	
+
 
 ans = True
 turtle.ht()
@@ -42,11 +55,13 @@ turtle.write("""
 		""",True, align="center", font=("Helvetica", 24, "bold italic"))
 def bye(event):
 	turtle.bye()
-for i in range (100):
+for i in range (10000):
 	turtle.getcanvas().bind("<Return>",bye)
 	turtle.getcanvas().bind("2",play)
 	turtle.getcanvas().bind("1",inst)
-	turtle.listen()	
+	turtle.listen()
+
+
 
 # <<<<<<< HEAD
 
--- menu_or.py
+++ menu_or.py
@@ -2,17 +2,31 @@ import turtle
 import time
 
 def inst(event):
-
-	inn=turtle.clone()
-	inn.home()
 	turtle.clear()
-	turtle.addshape("in.gif")
-	inn.shape("in.gif")
-	inn.stamp()
+	turtle.goto(250,-250)
+	turtle.write("""
+		A BRICK WALL APPEARS AT THE SCREEN 
+		AND OUR MISSION IS TO SMASH IT-
+		ONE BREAK AT A TIME!
+
+		*use the mouse to move the platform
+		across the bottom screen
+
+		*across the bottom screen
+		each time the ball hits a break, 
+		the brick disappears 
+		and you score points
+
+		2. PLAY!!
+
+		PRESS ENTER TO QUIT
+		""",True, align="right", font=("Helvetica", 15,"bold italic"))
 	turtle.update()
 def play(event):
 	turtle.clear()
 	turtle.update()
+	
+
 
 ans = True
 turtle.ht()
@@ -30,10 +44,12 @@ turtle.write("""
 		""",True, align="center", font=("Helvetica", 24, "bold italic"))
 def bye(event):
 	turtle.bye()
+for i in range (10000):
+	turtle.getcanvas().bind("<Return>",bye)
+	turtle.getcanvas().bind("2",play)
+	turtle.getcanvas().bind("1",inst)
+	turtle.listen()
 
-turtle.getcanvas().bind("<Return>",bye)
-turtle.getcanvas().bind("2",play)
-turtle.getcanvas().bind("1",inst)
-turtle.listen()
+
 
 turtle.mainloop()
